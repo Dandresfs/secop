3200f3a9a7a7eb73c0ebc9812e9c7645fbf414a7
Dandresfs <=>
2016-01-06 13:18:15 -0500

    Admin rss final

--- rss/admin.py
+++ rss/admin.py
@@ -3,15 +3,42 @@ from .models import Grupo, Segmento, LinkRss
 
 # Register your models here.
 class GrupoAdmin(admin.ModelAdmin):
-    list_display = ('codigo','nombre')
+    list_display = ['codigo','nombre']
 admin.site.register(Grupo, GrupoAdmin)
 
 class SegmentoAdmin(admin.ModelAdmin):
-    list_display = ('segmento','grupo__codigo','grupo__nombre','nombre')
-    list_filter = ('grupo__codigo',)
+    list_display = ['segmento','get_codigo_grupo','get_nombre_grupo','get_segmento_nombre']
+    list_filter = ['grupo__codigo']
+    search_fields = ['segmento']
+
+    def get_codigo_grupo(self,obj):
+        return obj.grupo.codigo
+    get_codigo_grupo.short_description = 'Codigo de Grupo'
+
+    def get_nombre_grupo(self,obj):
+        return obj.grupo.nombre
+    get_nombre_grupo.short_description = 'Nombre de Grupo'
+
+    def get_segmento_nombre(self,obj):
+        return obj.nombre
+    get_segmento_nombre.short_description = 'Nombre de Segmento'
+
 admin.site.register(Segmento, SegmentoAdmin)
 
 class LinkRssAdmin(admin.ModelAdmin):
-    list_display = ('segmento','segmento__nombre','segmento__grupo__codigo','link')
-    list_filter = ('segmento__grupo__codigo','segmento__segmento')
-admin.site.register(LinkRss)
+    list_display = ['segmento','get_codigo_grupo','get_nombre_grupo','get_segmento_nombre','link']
+    list_filter = ['segmento__grupo__codigo']
+    search_fields = ['segmento__segmento']
+
+    def get_codigo_grupo(self,obj):
+        return obj.segmento.grupo.codigo
+    get_codigo_grupo.short_description = 'Codigo de Grupo'
+
+    def get_nombre_grupo(self,obj):
+        return obj.segmento.grupo.nombre
+    get_nombre_grupo.short_description = 'Nombre de Grupo'
+
+    def get_segmento_nombre(self,obj):
+        return obj.segmento.nombre
+    get_segmento_nombre.short_description = 'Nombre de Segmento'
+admin.site.register(LinkRss,LinkRssAdmin)
