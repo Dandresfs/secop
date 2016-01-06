d5cd5b7a9efd3668c074d0d5e7e7b3694132c3c4
Dandresfs <=>
2016-01-06 11:16:34 -0500

    Creacion modelos basicos

---
+++ rss/models.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Grupo(models.Model):
+    codigo = models.CharField(max_length=1)
+    nombre = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.codigo
+
+class Segmento(models.Model):
+    grupo = models.ForeignKey(Grupo)
+    segmento = models.IntegerField()
+    nombre = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.segmento
+
+class LinkRss(models.Model):
+    segmento = models.ForeignKey(Segmento)
+    link = models.URLField()
+
+    def __unicode__(self):
+        return self.segmento
