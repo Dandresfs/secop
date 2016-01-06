2ff2cde3ff985305fcfd6b7bbd0176c9b68d29ad
Dandresfs <=>
2016-01-06 12:45:41 -0500

    Admin rss

--- rss/admin.py
+++ rss/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import Grupo, Segmento, LinkRss
 
 # Register your models here.
-admin.site.register(Grupo)
-admin.site.register(Segmento)
+class GrupoAdmin(admin.ModelAdmin):
+    list_display = ('codigo','nombre')
+admin.site.register(Grupo, GrupoAdmin)
+
+class SegmentoAdmin(admin.ModelAdmin):
+    list_display = ('segmento','grupo__codigo','grupo__nombre','nombre')
+    list_filter = ('grupo__codigo',)
+admin.site.register(Segmento, SegmentoAdmin)
+
+class LinkRssAdmin(admin.ModelAdmin):
+    list_display = ('segmento','segmento__nombre','segmento__grupo__codigo','link')
+    list_filter = ('segmento__grupo__codigo','segmento__segmento')
 admin.site.register(LinkRss)
