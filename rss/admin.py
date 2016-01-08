c94b17168b9d6563b5d279fda1c60fd8596d8bf0
Dandresfs <=>
2016-01-08 14:05:28 -0500

    Creacion de Modelos

--- rss/admin.py
+++ rss/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Grupo, Segmento, LinkRss
+from .models import Grupo, Segmento, Familia, Clase, Departamento, Municipio, TipoProceso, EstadoProceso, LinkRss, Proceso
 
 # Register your models here.
 class GrupoAdmin(admin.ModelAdmin):
@@ -22,8 +22,80 @@ class SegmentoAdmin(admin.ModelAdmin):
     def get_segmento_nombre(self,obj):
         return obj.nombre
     get_segmento_nombre.short_description = 'Nombre de Segmento'
-
 admin.site.register(Segmento, SegmentoAdmin)
+
+class FamiliaAdmin(admin.ModelAdmin):
+    list_display = ['familia','get_codigo_grupo','get_nombre_grupo','get_segmento_nombre','get_familia_nombre']
+    list_filter = ['segmento__grupo__codigo','segmento__nombre']
+    search_fields = ['familia']
+
+    def get_codigo_grupo(self,obj):
+        return obj.segmento.grupo.codigo
+    get_codigo_grupo.short_description = 'Codigo de Grupo'
+
+    def get_nombre_grupo(self,obj):
+        return obj.segmento.grupo.nombre
+    get_nombre_grupo.short_description = 'Nombre de Grupo'
+
+    def get_segmento_nombre(self,obj):
+        return obj.segmento.nombre
+    get_segmento_nombre.short_description = 'Nombre de Segmento'
+
+    def get_familia_nombre(self,obj):
+        return obj.nombre
+    get_familia_nombre.short_description = 'Nombre de Familia'
+admin.site.register(Familia, FamiliaAdmin)
+
+class ClaseAdmin(admin.ModelAdmin):
+    list_display = ['clase','get_codigo_grupo','get_nombre_grupo','get_segmento_nombre','get_familia_nombre','get_clase_nombre']
+    list_filter = ['familia__segmento__grupo__codigo','familia__segmento__nombre','familia__nombre']
+    search_fields = ['clase']
+
+    def get_codigo_grupo(self,obj):
+        return obj.familia.segmento.grupo.codigo
+    get_codigo_grupo.short_description = 'Codigo de Grupo'
+
+    def get_nombre_grupo(self,obj):
+        return obj.familia.segmento.grupo.nombre
+    get_nombre_grupo.short_description = 'Nombre de Grupo'
+
+    def get_segmento_nombre(self,obj):
+        return obj.familia.segmento.nombre
+    get_segmento_nombre.short_description = 'Nombre de Segmento'
+
+    def get_familia_nombre(self,obj):
+        return obj.familia.nombre
+    get_familia_nombre.short_description = 'Nombre de Familia'
+
+    def get_clase_nombre(self,obj):
+        return obj.familia.nombre
+    get_clase_nombre.short_description = 'Nombre de Clase'
+admin.site.register(Clase, ClaseAdmin)
+
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ['nombre']
+    search_fields = ['nombre']
+admin.site.register(Departamento, DepartamentoAdmin)
+
+class MunicipioAdmin(admin.ModelAdmin):
+    list_display = ['nombre','get_departamento_nombre']
+    list_filter = ['departamento__nombre']
+    search_fields = ['clase']
+
+    def get_departamento_nombre(self,obj):
+        return obj.departamento.nombre
+    get_departamento_nombre.short_description = 'Departamento'
+admin.site.register(Municipio, MunicipioAdmin)
+
+class TipoProcesoAdmin(admin.ModelAdmin):
+    list_display = ['tipo_proceso']
+    search_fields = ['tipo_proceso']
+admin.site.register(TipoProceso, TipoProcesoAdmin)
+
+class EstadoProcesoAdmin(admin.ModelAdmin):
+    list_display = ['estado_proceso']
+    search_fields = ['estado_proceso']
+admin.site.register(EstadoProceso, EstadoProcesoAdmin)
 
 class LinkRssAdmin(admin.ModelAdmin):
     list_display = ['segmento','get_codigo_grupo','get_nombre_grupo','get_segmento_nombre','get_link']
--- rss/models.py
+++ rss/models.py
@@ -27,6 +27,70 @@ class Segmento(models.Model):
         verbose_name = "Segmento"
         verbose_name_plural = "Segmentos"
 
+class Familia(models.Model):
+    segmento = models.ForeignKey(Segmento)
+    familia = models.IntegerField()
+    nombre = models.CharField(max_length=500)
+
+    def __unicode__(self):
+        return str(self.familia)
+
+    class Meta:
+        verbose_name = "Familia"
+        verbose_name_plural = "Familias"
+
+class Clase(models.Model):
+    familia = models.ForeignKey(Familia)
+    clase = models.IntegerField()
+    nombre = models.CharField(max_length=500)
+
+    def __unicode__(self):
+        return str(self.clase)
+
+    class Meta:
+        verbose_name = "Clase"
+        verbose_name_plural = "Clases"
+
+class Departamento(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return str(self.nombre)
+
+    class Meta:
+        verbose_name = "Departamento"
+        verbose_name_plural = "Departamentos"
+
+class Municipio(models.Model):
+    departamento = models.ForeignKey(Departamento)
+    nombre = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return str(self.nombre)
+
+    class Meta:
+        verbose_name = "Municipio"
+        verbose_name_plural = "Municipios"
+
+class TipoProceso(models.Model):
+    tipo_proceso = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return str(self.tipo_proceso)
+
+    class Meta:
+        verbose_name = "Tipo de Proceso"
+        verbose_name_plural = "Tipos de Procesos"
+
+class EstadoProceso(models.Model):
+    estado_proceso = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return str(self.estado_proceso)
+
+    class Meta:
+        verbose_name = "Estado de Proceso"
+        verbose_name_plural = "Estados de Procesos"
 
 class LinkRss(models.Model):
     segmento = models.ForeignKey(Segmento)
@@ -38,3 +102,31 @@ class LinkRss(models.Model):
     class Meta:
         verbose_name = "Link RSS"
         verbose_name_plural = "Link RSS"
+
+class Proceso(models.Model):
+    #---------------- Atributos del canal RSS--------------
+    pubdate = models.DateTimeField()
+    title = models.CharField(max_length=500)
+    description = models.TextField(max_length=10000)
+    link = models.URLField()
+    author = models.EmailField()
+    category = models.CharField(max_length=100)
+    cuantia = models.DecimalField()
+    departamento = models.ForeignKey(Departamento)
+    municipio = models.ForeignKey(Municipio)
+    #-------------------Atributos Pagina Web---------------
+    tipo_proceso = models.ForeignKey(TipoProceso)
+    estado_proceso = models.ForeignKey(EstadoProceso)
+    grupo = models.ForeignKey(Grupo)
+    segmento = models.ForeignKey(Segmento)
+    familia = models.ForeignKey(Familia)
+    clase = models.ForeignKey(Clase)
+    objeto = models.TextField(max_length=10000)
+    tipo_contrato = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return str(self.segmento)
+
+    class Meta:
+        verbose_name = "Proceso"
+        verbose_name_plural = "Procesos"
